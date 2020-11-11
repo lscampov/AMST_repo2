77aa060a8268494edfdfaebe7fb0b534b1b65cd1
rsserran <73566886+rsserran@users.noreply.github.com>
2020-11-10 19:33:04 -0500

    CalendarView

--- app/src/main/java/com/example/amst_g5/MainActivity.java
+++ app/src/main/java/com/example/amst_g5/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.amst_g5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView theDate;
+    private TextView theEvent;
+    private Button btnGoToCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        theDate= (TextView) findViewById(R.id.date);
+        btnGoToCalendar=(Button) findViewById(R.id.btnGoCalendar);
+        theEvent= (TextView) findViewById(R.id.event);
+
+        btnGoToCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
